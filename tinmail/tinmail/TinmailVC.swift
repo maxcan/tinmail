ab11b34154b9a665736e91208d6f4f26adf37b08
Max Cantor (MBPr) <i@cantor.mx>
2014-06-29 22:03:58 -0700

    ok, making progress

--- tinmail/tinmail/TinmailVC.swift
+++ tinmail/tinmail/TinmailVC.swift
@@ -8,50 +8,74 @@
 
 import UIKit
 
-
 class TinmailVC: UIViewController {
     
+    var auth:GTMOAuth2Authentication?
+    let kKeychainItemName = "OAuth2 Sample: Google+"
+    let kMyClientID = "241491780934-na17dn4btvf2m2843cgvic8n6s0l13vc.apps.googleusercontent.com"    // pre-assigned by service
+    let kMyClientSecret = "mWx1B6A9RonGSIxI6m21itn9" // pre-assigned by service
+    
+    //    init(coder aDecoder: NSCoder!) {
+    //        auth = nil
+    //        super(coder)
+    //    }
     override func viewDidLoad() {
+        
+        self.auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychainForName(kKeychainItemName,
+            clientID:kMyClientID,
+            clientSecret:kMyClientSecret);
+        
         super.viewDidLoad()
         
-        
-        
-        
-        
-        //        finishedSelector:@selector(viewController:finishedWithAuth:error:)] autorelease];
+        // Retain the authentication object, which holds the auth tokens
         //
-        //        [[self navigationController] pushViewController: animated:YES];
+        // We can determine later if the auth object contains an access token
+        // by calling its -canAuthorize method
+        //                [self setAuthentication:auth];
     }
-    
-    // typedef void (^GTMOAuth2ViewControllerCompletionHandler)(GTMOAuth2ViewControllerTouch *viewController, GTMOAuth2Authentication *auth, NSError *error);
     
     
     @IBAction func startOauth(sender: AnyObject) {
-        
+//        if (auth != nil) {
+//            println("already have it")
+//            return
+//        }
         println("init oauthvc")
         NSLog("init oa")
-        let kKeychainItemName = "OAuth2 Sample: Google+"
-        
-        let kMyClientID = "241491780934-na17dn4btvf2m2843cgvic8n6s0l13vc.apps.googleusercontent.com"    // pre-assigned by service
-        let kMyClientSecret = "mWx1B6A9RonGSIxI6m21itn9" // pre-assigned by service
-        
-        let scope = "https://www.googleapis.com/auth/plus.me" // scope for Google+ API
-        
+        let scope = "https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.modify" // scope for Google+ API
+        //        self.onOauthSucc(nil, auth: nil, err: nil)
         // GTMOAuth2ViewControllerTouch *viewController;
         var gtmVC: GTMOAuth2ViewControllerTouch = GTMOAuth2ViewControllerTouch(scope:scope,
             clientID:kMyClientID,
             clientSecret:kMyClientSecret,
             keychainItemName:kKeychainItemName,
-            delegate:nil,
-            finishedSelector:Selector("onOauthSucc::"))
+            delegate:self,
+            finishedSelector:Selector("succ:auth:err:")
+        )
         self.navigationController?.pushViewController(gtmVC, animated: true)
         
     }
-    func onOauthSucc(auth: GTMOAuth2Authentication, err: NSError) {
-        NSLog("success", auth)
+    func succ(sender:UIViewController, auth: GTMOAuth2Authentication?, err: NSError){
+        println("success")
+        self.auth = auth
+        getUserId()
     }
-    
-    
+    func getUserId() {
+        if let a = auth {
+            let userIdUrl = "https://www.googleapis.com/gmail/v1/users/me/messages"
+            var fetcher:GTMHTTPFetcher = GTMHTTPFetcher(URLString: userIdUrl)
+            fetcher.authorizer = a
+//            fetcher.beginFetchWithCompletionHandler(<#handler: ((NSData!, NSError!) -> Void)?#>)
+            fetcher.beginFetchWithCompletionHandler({ s1, s2 in
+                if (s2 != nil) {
+                    println("erorr", s2)
+                }
+                let jsonDict = NSJSONSerialization.JSONObjectWithData(s1, options: nil, error: nil) as NSDictionary
+                println(jsonDict)
+        })
+        
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
